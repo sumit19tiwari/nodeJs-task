058bb51dcaa80719ac501038448927be6c2dea7e
sumit19tiwari <36476618+sumit19tiwari@users.noreply.github.com>
2022-12-12 15:43:30 +0530

    Update stock.test.ts

--- src/test/stock.test.ts
+++ src/test/stock.test.ts
@@ -1,5 +1,6 @@
 import router from '../index';
 import request from "supertest";
+import stockController from '../controller/stock-info';
 
 describe("GET /get-stock/:sku(*)'", () => {
     it("should return current quantity based on sku", async () => {
@@ -11,5 +12,17 @@ describe("GET /get-stock/:sku(*)'", () => {
       const result = await request(router).get("/get-stock/IQZ340003/37/31");
       expect(result.statusCode).toEqual(200);
       expect(result.text).toEqual("{\"msg\":\"No sku found\"}");
+    });
+  });
+
+ 
+  describe("Find current stock", () => {
+    it("should return current stock", async()=>{
+      const expectResult = { qty: 8517, sku: "LTV719449/39/39"};
+      expect(stockController.getCurrentStock("LTV719449/39/39")).resolves.toEqual(expectResult);
+    });
+    it("should return no sku found", async()=>{
+      const expectResult = {msg: 'No sku found'};
+      expect(stockController.getCurrentStock("LTV719449/39/391")).resolves.toEqual(expectResult);
     });
   });
