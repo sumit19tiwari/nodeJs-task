e4b4fcc39a63a9fac8bfca7c6b19e1e775a61849
sumit19tiwari <36476618+sumit19tiwari@users.noreply.github.com>
2022-12-15 16:24:34 +0530

    Update stock.test.ts

--- src/test/stock.test.ts
+++ src/test/stock.test.ts
@@ -25,4 +25,16 @@ describe("GET /get-stock/:sku(*)'", () => {
       const expectResult = {msg: 'No sku found'};
       expect(stockController.getCurrentStock("LTV719449/39/391")).resolves.toEqual(expectResult);
     });
+    it("should subtract the quantity when the transaction type is ORDER", async()=>{
+      const stockData = [{"sku":"CLQ274846/07/46","stock":8414}];
+      const transactionData = [{"sku":"CLQ274846/07/46","type":"order","qty":6},{"sku":"CLQ274846/07/46","type":"order","qty":10}];
+      const currentStock = stockController.calculateCurrentStock("CLQ274846/07/46", stockData, transactionData);
+      expect(currentStock).toEqual({qty: 8398, sku: "CLQ274846/07/46"});
+    })
+    it("should add the quantity when the transaction type is REFUND", async()=>{
+      const stockData = [{"sku":"CLQ274846/07/46","stock":8414}];
+      const transactionData = [{"sku":"CLQ274846/07/46","type":"refund","qty":6},{"sku":"CLQ274846/07/46","type":"refund","qty":10}];
+      const currentStock = stockController.calculateCurrentStock("CLQ274846/07/46", stockData, transactionData);
+      expect(currentStock).toEqual({qty: 8430, sku: "CLQ274846/07/46"});
+    })
   });
